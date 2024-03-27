82b52d40a59866a2a534c4d7fe9b4c8ede22083b
Praba sri <prabasri1998@gmail.com>
2024-03-27 18:53:55 +0530

    Testing

--- src/FullName/Fullname.jsx
+++ src/FullName/Fullname.jsx
@@ -17,10 +17,10 @@ function Fullname() {
     <form onSubmit={handleSubmit}>
       <h1>Full Name Display</h1>
       <label htmlFor="firstName">First Name:</label>  {/* To display 'First Name:' near input box and don't use <p> tag for this */}
-      <input type="text" id="firstName" onChange={(e) => {setFirstName(e.target.value)}} value={firstName} required/> {/* */}
+      <input type="text" id="firstName" value={firstName} onChange={(e) => {setFirstName(e.target.value)}} required/> {/* */}
       <br/>
       <label htmlFor="lastName">Last Name:</label> {/* The htmlFor prop in JSX is the same as the for attribute in HTML */}
-      <input type="text" id="lastName" onChange={(e) => {setLastName(e.target.value)}} value={lastName} required/>
+      <input type="text" id="lastName" value={lastName} onChange={(e) => {setLastName(e.target.value)}} required/>
       <br/>
       <button type="submit">Submit</button> {/* The <input type="submit"> defines a submit button which submits all form values to a form-handler. The form-handler is typically a server page with a script for processing the input data. The form-handler is specified in the form's action attribute */}
       <p>{isSubmitted ? `Full Name: ${fullName}` : ""}</p>
