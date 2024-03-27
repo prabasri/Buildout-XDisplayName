6e5a023a23c980a262ea655646c8002b55864b65
Praba sri <prabasri1998@gmail.com>
2024-03-27 22:00:43 +0530

    Written output out of form

--- src/FullName/Fullname.jsx
+++ src/FullName/Fullname.jsx
@@ -10,7 +10,6 @@ function Fullname() {
 
   const handleSubmit = (event) => {
     event.preventDefault();  // to prevent the default behaviour of any element like button, form, anchor tag, etc
-    // console.log(fullName);
     setIsSubmitted(true);
   }
 
@@ -19,18 +18,17 @@ function Fullname() {
       <form onSubmit={handleSubmit}>
         <h1>Full Name Display</h1>
         <label htmlFor="firstName">First Name:</label>  {/* To display 'First Name:' near input box and don't use <p> tag for this */}
-        <input type="text" id="firstName" value={firstName} onChange={(e) => {setFirstName(e.target.value)}} required/> {/* */}
+        <input type="text" id="firstName" value={firstName} onChange={(e) => {setFirstName(e.target.value)}} required/>
         <br/>
         <label htmlFor="lastName">Last Name:</label> {/* The htmlFor prop in JSX is the same as the for attribute in HTML */}
         <input type="text" id="lastName" value={lastName} onChange={(e) => {setLastName(e.target.value)}} required/>
         <br/>
         <button type="submit">Submit</button> {/* The <input type="submit"> defines a submit button which submits all form values to a form-handler. The form-handler is typically a server page with a script for processing the input data. The form-handler is specified in the form's action attribute */}
-        
-        {/* {isSubmitted && <p>Full Name: {fullName}}</p> Don't use p tag inside */}
+
+        {/* {isSubmitted && <p>Full Name: {fullName}}</p>     Don't use this p tag inside the form, it will fail the testcases. */}
       </form>
       {isSubmitted && <p>Full Name: {fullName}</p>}
     </div>
-    
   )
 }
 
